8da5c441e2447ecab6dcca65c1adde4060295a7f
luizhcb0 <luizhcb0@gmail.com>
2017-04-07 02:18:45 -0300

    Configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static#index"
+  # resources :static
+  get "/get_time", to: "static#get_time", as: "get_time"
+  
+  # Para funcionar, tem que mudar no JS para static/get_time
+  # get 'static/index'
+  # get 'static/get_time'
+
+
 end
