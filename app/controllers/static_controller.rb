906ffdf24e0f2b3642405b222c8865c2256d6a16
luizhcb0 <luizhcb0@gmail.com>
2017-04-07 02:20:19 -0300

    Controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,2 +1,12 @@
 class StaticController < ApplicationController
+  
+  def index
+    @time = DateTime.now
+  end
+  
+  def get_time
+    @time = DateTime.now
+    render partial: "date"
+  end
+  
 end
